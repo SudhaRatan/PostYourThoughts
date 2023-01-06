f59790bda0e32d344b22dbb52668699ecec13f3b
SudhaRatan <rattankumar2511@gmail.com>
2023-01-06 23:20:27 +0530

    3rd version

--- src/pages/post.js
+++ src/pages/post.js
@@ -4,7 +4,10 @@ import "./pageStyle.css"
 const Post = () => {
     return (
         <div className="post-form">
-        <h1>Post your thoughts</h1>
+            <h1>Post your thoughts</h1>
+            <div>
+                Please post landscape images instead of portrait
+            </div>
             <PostForm />
         </div>
     )
